f1be421149e2d262ce8eeb13c266dae15b08efca
mrit1411 <130552115+mrit1411@users.noreply.github.com>
2025-07-21 13:08:45 +0530

    Update locustfile.py

--- locustfile.py
+++ locustfile.py
@@ -3,7 +3,7 @@ from locust import HttpUser, task
 class OllamaUser(HttpUser):
     @task
     def query_model(self):
-        self.client.post("/api/generate", json={
+        self.client.post("/", json={
             "model": "gemma:2b",
             "prompt": "Summarize AI in 3 lines."
         })
