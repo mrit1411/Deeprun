b3b5740cc2e7a097c18986048770513154ca6f19
mrit1411 <130552115+mrit1411@users.noreply.github.com>
2025-07-21 13:48:12 +0530

    Update locustfile.py

--- locustfile.py
+++ locustfile.py
@@ -3,4 +3,4 @@ from locust import HttpUser, task
 class OllamaUser(HttpUser):
     @task
     def query_model(self):
-        self.client.post("/", json={"prompt": "Summarize AI in 3 lines."})
+        self.client.post("/", json={"prompt": "Hello."})
