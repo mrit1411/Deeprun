77333e8a32ab4c91359b27d046ef282ff911922b
mrit1411 <130552115+mrit1411@users.noreply.github.com>
2025-07-21 13:12:45 +0530

    Update locustfile.py

--- locustfile.py
+++ locustfile.py
@@ -3,7 +3,9 @@ from locust import HttpUser, task
 class OllamaUser(HttpUser):
     @task
     def query_model(self):
-        self.client.post("/", json={
+        self.client.post("/api/chat", json={
             "model": "gemma:2b",
-            "prompt": "Summarize AI in 3 lines."
+            "messages": [
+                { "role": "user", "content": "Summarize AI in 3 lines." }
+            ]
         })
